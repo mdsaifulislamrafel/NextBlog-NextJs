444eb7ff0613af5560730b902050cf09d0d8b2c2
Md Saiful Islam Rafel <mdsaifulisalmrafel.com>
2025-03-03 02:21:22 +0600

    create blog and dynamic title added

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,5 +1,6 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
 
+// সাইড লোড হওয়ার সময় কিছু ডাটা আগে থেকেই লোড করে নিবে
 export const generateStaticParams = async () => {
   const res = await fetch("http://localhost:5000/blogs");
   const blogs = await res.json();
@@ -8,6 +9,21 @@ export const generateStaticParams = async () => {
     blogId: post.id,
   }));
 };
+
+// ডায়নামিক টাইটেল পরিবর্তন ফাংশন
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+  return {
+    title: blog.title,
+    description: blog.description,
+  };
+}
 
 const BlogDetailsPage = async ({
   params,
